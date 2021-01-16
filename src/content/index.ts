28d55f3e0d995743e9d1b65827f38bc295eaa8cc
Philip Capel <insert.available.name@gmail.com>
2021-01-15 21:25:23 -0600

    Persist note content

--- src/content/index.ts
+++ src/content/index.ts
@@ -16,20 +16,24 @@ type Id = number;
 
 type PageState = {
   noteIds: Id[];
-  notePositions: [Id, Position][];
+  notePositions: [Position][];
+  noteContent: string[];
 };
 
 const addBasicNote = createAction('ADD_BASIC_NOTE');
 const dragStopPositionChange = createAction('DRAG_STOP_POSITION_CHANGE');
+const noteContentUpdate = createAction('NOTE_CONTENT_UPDATE');
 
 function pageReducer(state: PageState, { type, data }: Action): PageState {
   if (state === undefined) {
     state = {
       noteIds: [],
       notePositions: [],
+      noteContent: [],
     };
   }
-  const { noteIds, notePositions } = state;
+  const { noteIds, notePositions, noteContent } = state;
+  const index = noteIds.indexOf(data.id);
   switch (type) {
     case addBasicNote.type:
       let newId;
@@ -41,12 +45,22 @@ function pageReducer(state: PageState, { type, data }: Action): PageState {
       return {
         noteIds: [...noteIds, newId],
         notePositions: [...notePositions, data],
+        noteContent: [...noteContent, ''],
       };
+
     case dragStopPositionChange.type:
-      const index = noteIds.indexOf(data.id);
       return {
         noteIds: [...noteIds],
         notePositions: replaceAtIndex(notePositions, index, data.position),
+        noteContent: [...noteContent],
+      };
+
+    case noteContentUpdate.type:
+      console.log(index, data, noteContent);
+      return {
+        noteIds: [...noteIds],
+        notePositions: [...notePositions],
+        noteContent: replaceAtIndex(noteContent, index, data.content),
       };
     default:
       return state;
@@ -94,9 +108,18 @@ const handleOnDrop = (note: HTMLElement) => (customEvent: CustomEvent) => {
   );
 };
 
-function placeNote(position: Position) {
-  const note = document.createElement('share-note');
-  (note as Note).updatePosition(...position);
+function placeNote(position: Position, content: String | undefined) {
+  const note = document.createElement('share-note') as Note;
+  note.updatePosition(...position);
+
+  note.input.addEventListener('change', (event: Event) => {
+    console.log('onchange', event);
+    // @ts-ignore
+    const content = event.currentTarget.value;
+    dispatch(noteContentUpdate({ id: parseInt(note.id), content }));
+  });
+  // @ts-ignore
+  note.input.value = !!content ? content : '';
   document.body.appendChild(note);
   return note;
 }
@@ -115,7 +138,7 @@ const dispatchNormalModeAction = (sequence: string): void => {
         anchorOffset: anchorOffset,
       });
 
-      const note = placeNote(position);
+      const note = placeNote(position, undefined);
 
       setTimeout(() => (note as Note).input.focus(), 20);
 
@@ -173,9 +196,9 @@ const selectionContextSet = (_event: any): void => {
 const placeExistingNotes = () => {
   const href = window.location.href;
   browser.storage.local.get(href).then((storage: any) => {
-    const { noteIds, notePositions } = storage[href];
+    const { noteIds, notePositions, noteContent } = storage[href];
     noteIds.forEach((id: string, index: number) => {
-      const note = placeNote(notePositions[index]);
+      const note = placeNote(notePositions[index], noteContent[index]);
       note.id = id;
       note.addEventListener('ondragstop', handleOnDrop(note));
     });
